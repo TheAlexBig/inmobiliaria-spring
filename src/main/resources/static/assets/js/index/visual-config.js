da05d739a72678c7dc936110f36c35662ed44c7b
TheAlexBig <00062816@uca.edu.sv>
2019-08-22 12:47:54 -0600

    Index reworked

--- src/main/resources/static/assets/js/index/visual-config.js
+++ src/main/resources/static/assets/js/index/visual-config.js
@@ -9,19 +9,9 @@ $(document).ready(function(){
     $('#intro-items').slick({
         centerMode: true,
         centerPadding: '60px',
-        slidesToShow: 4,
-        arrows: false,
-        responsive: [
-            {
-                breakpoint: 768,
-                settings: {
-                    arrows: false,
-                    centerMode: true,
-                    centerPadding: '40px',
-                    slidesToShow: 4
-                }
-            }
-            ]
+        slidesToShow: 3,
+        prevArrow: $('.prev-slick'),
+        nextArrow: $('.next-slick')
     });
 
     function setPopoverMenu() {
