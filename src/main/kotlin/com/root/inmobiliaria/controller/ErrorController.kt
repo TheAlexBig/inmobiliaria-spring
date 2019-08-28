57e797fdd20e093155bb5981c2abdf996b9ad0b4
TheAlexBig <00062816@uca.edu.sv>
2019-08-27 23:13:23 -0600

    Loader removed

---
+++ src/main/kotlin/com/root/inmobiliaria/controller/ErrorController.kt
@@ -0,0 +1,29 @@
+package com.root.inmobiliaria.controller
+
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.boot.web.servlet.error.ErrorController
+import org.springframework.stereotype.Controller
+import org.springframework.http.HttpStatus
+import org.springframework.ui.Model
+import javax.servlet.RequestDispatcher
+import javax.servlet.http.HttpServletRequest
+
+
+@Controller
+class ErrorController : ErrorController{
+
+    @RequestMapping("/error")
+    fun handleError(request: HttpServletRequest, model : Model): String {
+        val status = request.getAttribute(RequestDispatcher.ERROR_STATUS_CODE)
+
+        if (status != null) {
+            model.addAttribute("error", Integer.valueOf(status.toString()))
+            return "client/error"
+        }
+        return "client/index"
+    }
+
+    override fun getErrorPath(): String {
+        return "client/error"
+    }
+}
