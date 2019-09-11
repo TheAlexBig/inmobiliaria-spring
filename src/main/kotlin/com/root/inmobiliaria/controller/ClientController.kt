47614053e5058c8ef4a4f5978ba55fa926e2a7fc
TheAlexBig <00062816@uca.edu.sv>
2019-09-10 18:51:37 -0600

    Revert "No view resolver error 2"
    This reverts commit 1314e0e8b7b9a741dacf5e4f712a2ee6d5c63640.

---
+++ src/main/kotlin/com/root/inmobiliaria/controller/ClientController.kt
@@ -0,0 +1,9 @@
+package com.root.inmobiliaria.controller
+
+import org.springframework.stereotype.Controller
+import org.springframework.web.bind.annotation.GetMapping
+
+@Controller
+class ClientController{
+
+}
